fcebf958232c5f31618cb907259c6cd526ab497f
Diyorbek Ismoilov <dismoilov@cdti.uz>
2025-05-16 11:44:17 +0500

    done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -54,6 +53,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    //Libraries need to implement
+    implementation(libs.androidx.appcompat)
 
     // EMV NFC Card library
     implementation ("com.github.devnied.emvnfccard:library:3.0.1")
